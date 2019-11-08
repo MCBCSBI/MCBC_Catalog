2afe608bf70f42191d9ef19e4d4cd6e22056d483
Yohaness Ramanah <yohaness.ramanah@mcbconsulting.mu>
2019-11-09 00:48:45 +0400

    modified dim customer  - not complete

--- MCBC_Catalog_SSMS_Solution/SQL Scripts/DW_Dim_Customer.sql
+++ MCBC_Catalog_SSMS_Solution/SQL Scripts/DW_Dim_Customer.sql
@@ -1,89 +1,47 @@
 --use insightlanding_sey
+declare @dt date = '2019-09-30';
 
+with c as(
 SELECT 
 	C.MIS_DATE
-	,c.BRANCH_CO_MNE
 	,C.[@ID] 
 	,C.Name_1
 	,C.Gender
-	,DAO.NAME AS Account_Officer
-	,C.Nationality
-	,c.RESIDENCE
-	,case when c.RESIDENCE = 'SC' then N'Resident' else N'Non-Resident' end as Residence_type
-	,C.Customer_Type
-	,CAST(C.Date_Of_Birth as DATE) Date_Of_Birth
 	,SEC.DESCRIPTION AS Sector
 	,tar.DESCRIPTION as Target
 	,INS.DESCRIPTION AS Industry
+	,C.Nationality
+	,c.RESIDENCE
+	,case when c.RESIDENCE = 'SC' then N'Resident' else N'Non-Resident' end as Residence_type
+	,DAO.NAME AS Account_Officer
+	,C.Customer_Type
+	,CAST(C.Date_Of_Birth as DATE) Date_Of_Birth
 	,C.Address
 	,C.LEGAL_ID
-	,C.CUSTOMER_SINCE
-	,CAST(C.Contact_Date as DATE) Contact_Date
-	,C.Introducer
-	,C.Kyc_Relationship
-	,CAST(C.Last_Kyc_Review_Date as DATE) Last_Kyc_Review_Date
-	,CAST(C.Auto_Next_Kyc_Review_Date as DATE) Auto_Next_Kyc_Review_Date
-	,CAST(C.Last_Suit_Review_Date as DATE) Last_Suit_Review_Date
-	,CAST(C.Auto_Next_Suit_Review_Date as DATE) Auto_Next_Suit_Review_Date
-	,CAST(C.Manual_Next_Kyc_Review_Date as DATE) Manual_Next_Kyc_Review_Date
-	,CAST(C.Manual_Next_Suit_Review_Date as DATE) Manual_Next_Suit_Review_Date
-	,C.Risk_Asset_Type
-	,C.Risk_Level
-	,C.Risk_Tolerance
-	,concat(left(C.Risk_From_Date,4),'-',substring(C.Risk_From_Date,5,2),'-',right(C.Risk_From_Date,2)) as Risk_From_Dates
-	,C.Aml_Check
-	,C.Aml_Result
-	,CAST(C.Last_Aml_Result_Date as DATE) Last_Aml_Result_Date
-	,C.Calc_Risk_Class
-	,C.Manual_Risk_Class
-	,C.Override_Reason
-	,C.Kyc_Complete
+	,cast(C.CUSTOMER_SINCE as date) as CUSTOMER_SINCE
+	--,CAST(C.Contact_Date as DATE) Contact_Date
+	--,C.Introducer
+	--,C.Kyc_Relationship
+	--,CAST(C.Last_Kyc_Review_Date as DATE) Last_Kyc_Review_Date
+	--,CAST(C.Auto_Next_Kyc_Review_Date as DATE) Auto_Next_Kyc_Review_Date
+	--,CAST(C.Last_Suit_Review_Date as DATE) Last_Suit_Review_Date
+	--,CAST(C.Auto_Next_Suit_Review_Date as DATE) Auto_Next_Suit_Review_Date
+	--,CAST(C.Manual_Next_Kyc_Review_Date as DATE) Manual_Next_Kyc_Review_Date
+	--,CAST(C.Manual_Next_Suit_Review_Date as DATE) Manual_Next_Suit_Review_Date
+	--,C.Risk_Asset_Type
+	--,C.Risk_Level
+	--,C.Risk_Tolerance
+	--,concat(left(C.Risk_From_Date,4),'-',substring(C.Risk_From_Date,5,2),'-',right(C.Risk_From_Date,2)) as Risk_From_Dates
+	--,C.Aml_Check
+	---,C.Aml_Result
+	--,CAST(C.Last_Aml_Result_Date as DATE) Last_Aml_Result_Date
+	--,C.Calc_Risk_Class
+	--,C.Manual_Risk_Class
+	--,C.Override_Reason
+	--,C.Kyc_Complete
 	,t01.value as Pref_Com_Channel
-	,C.COMPANY_BOOK
-	,b.company_name
-    ,[Scd2_Hash] =
-		(isnull(convert(char(66),HASHBYTES('SHA2_256',concat(
-			C.[@ID] 
-			,C.Name_1
-			,c.BRANCH_CO_MNE
-			,sec.DESCRIPTION
-			,INS.DESCRIPTION
-			,tar.DESCRIPTION
-			,C.Gender
-			,C.Account_Officer
-			,C.Nationality
-			,c.RESIDENCE
-			,c.Residence_type
-			,C.Customer_Type
-			,C.Date_Of_Birth
-			,C.Address
-			,C.LEGAL_ID
-			,C.CUSTOMER_SINCE
-			,C.Contact_Date
-			,C.Introducer
-			--,CLREF.L_Kyc_Document
-			,C.Kyc_Relationship
-			,C.Last_Kyc_Review_Date
-			,C.Auto_Next_Kyc_Review_Date
-			,C.Last_Suit_Review_Date
-			,C.Auto_Next_Suit_Review_Date
-			,C.Manual_Next_Kyc_Review_Date
-			,C.Manual_Next_Suit_Review_Date
-			,C.Risk_Asset_Type
-			,C.Risk_Level
-			,C.Risk_Tolerance
-			,C.Risk_From_Date
-			,C.Aml_Check
-			,C.Aml_Result
-			,C.Last_Aml_Result_Date
-			,C.Calc_Risk_Class
-			,C.Manual_Risk_Class
-			,C.Override_Reason
-			,C.Kyc_Complete
-			,t01.value
-			,c.COMPANY_BOOK
-			,b.company_name
-		)),1),0))
+	,C.COMPANY_BOOK as BRANCH_Code
+	,b.company_name as branch_name
 		
 FROM 
 	[dbo].[BNK_CUSTOMER] C
@@ -95,10 +53,6 @@ FROM
 	LEFT JOIN dbo.BNK_SECTOR SEC
 		ON SEC.MIS_DATE = C.MIS_DATE 
 		AND C.SECTOR = SEC.[@ID]
-
-	LEFT JOIN dbo.BNK_LANGUAGE LAN
-		ON LAN.MIS_DATE = C.MIS_DATE
-		AND C.LANGUAGE = LAN.[@ID]
 
 	LEFT JOIN dbo.BNK_COUNTRY COUN
 		ON COUN.MIS_DATE = C.MIS_DATE
@@ -127,4 +81,31 @@ FROM
 			,ROW_NUMBER() over (order by c.LOCAL_REF) as rw
 		from string_split(c.LOCAL_REF,nchar(63741)) t
 	)t01
-	where t01.rw in (23) and c.mis_date = '2019-09-30'
+	where t01.rw in (23) and c.mis_date = @dt
+)
+
+
+/*MAIN SELECT*/
+select
+	c.MIS_DATE,
+	c.[@ID]
+	,c.NAME_1
+	,c.Sector
+	,c.Target
+	,c.Industry
+	,c.BRANCH_Code
+	,c.BRANCH_NAME
+	,c.CUSTOMER_SINCE
+	,[Scd2_Hash] =(isnull(convert(char(66),HASHBYTES('SHA2_256',concat(
+		c.[@ID]
+	,c.NAME_1
+	,c.Sector
+	,c.Target
+	,c.Industry
+	,c.BRANCH_Code
+	,c.BRANCH_NAME
+	,c.CUSTOMER_SINCE
+		)),1),0))
+
+from c
+
