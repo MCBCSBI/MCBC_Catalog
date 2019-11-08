bbb1b3f56742325c52ec3c1e3cd7d91d92ad923c
Yohaness Ramanah <yohaness.ramanah@mcbconsulting.mu>
2019-11-08 11:28:59 +0400

    archived old sql files
    created Measure Table

---
+++ MCBC_Catalog_SSMS_Solution/SQL Scripts/ddl_Measure_Table.sql
@@ -0,0 +1,25 @@
+
+/****** Object:  Table [dbo].[Measure_Table]    Script Date: 08/11/2019 11:23:14 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+drop table if exists measure_table
+
+CREATE TABLE [dbo].[Measure_Table](
+	[m1] [nvarchar](10) NULL,
+	[m2] [nvarchar](10) NULL,
+	[m3] [nvarchar](10) NULL,
+	[m4] [nvarchar](10) NULL,
+	[m5] [nvarchar](10) NULL,
+	[m6] [nvarchar](10) NULL,
+	[m7] [nvarchar](10) NULL,
+	[m8] [nvarchar](10) NULL,
+	[m9] [nvarchar](10) NULL,
+	[m10] [nvarchar](10) NULL
+) ON [PRIMARY]
+GO
+
+
